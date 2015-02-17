681494aaa3fec81d6d50e3d9960efb697a29cf8a
sachour <sachour@mit.edu>
2015-02-17 14:14:14 -0500

    test2

--- gen_env.sh
+++ gen_env.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 CURRDIR=$(pwd)
-ln -s "env/jshint_precommit_hook.sh" .git/hooks/pre-commit
+
+rm .git/hooks/pre-commit
+ln -s env/jshint_precommit_hook.sh .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
+
 echo "#!/bin/bash" > env.sh
 echo "export UI_PROJ_DIR='$CURRDIR'" >> env.sh
 
