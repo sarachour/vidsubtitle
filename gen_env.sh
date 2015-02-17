21a266040efa7628fffd00a1d9c765f08d89b84f
sachour <sachour@mit.edu>
2015-02-17 14:18:50 -0500

    fixed script

--- gen_env.sh
+++ gen_env.sh
@@ -3,7 +3,7 @@
 CURRDIR=$(pwd)
 
 rm .git/hooks/pre-commit
-ln -s env/jshint_precommit_hook.sh .git/hooks/pre-commit
+cp env/jshint_precommit_hook.sh .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
 
 echo "#!/bin/bash" > env.sh
