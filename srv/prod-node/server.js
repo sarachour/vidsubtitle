557d21c003e9c3dc9c5669f032c47b30f4986174
sachour <sachour@mit.edu>
2015-04-27 10:16:53 -0400

    added dummy hooks for database

--- srv/prod-node/server.js
+++ srv/prod-node/server.js
@@ -1,19 +1,22 @@
-var static = require('node-static')
+var srv_static = require('node-static')
 var fs = require('fs');
+var mongo= {};
+mongo.client = require('mongodb').MongoClient;
+mongo.server = require('mongodb').Server;
+mongo.db = require('mongodb').Db;
 
-var file = new static.Server('../../site/',
-   {
-      headers: {
-         "Access-Control-Allow-Origin":"*",
-         "Access-Control-Allow-Headers":"X-Requested-With"
-      }
-   });
+
+var file = new srv_static.Server('../../site/',
+{
+   headers: {
+      "Access-Control-Allow-Origin":"*",
+      "Access-Control-Allow-Headers":"X-Requested-With"
+   }
+});
 
 var options = {
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem')
-   //key: fs.readFileSync('server.key'),
-   //cert: fs.readFileSync('server.crt')
 };
 
 var process_file = function(req,res){
@@ -33,3 +36,18 @@ require('http').createServer(function (req, res) {
   process_file(req,res);
 }).listen(8080);
 
+/*
+var db = new mongo.db('subs', new Server('localhost', 27017))
+*/
+/*
+var mongo_base_url = 'mongodb://localhost:12345'
+var mongo_sub_url = mongo_base_url + "/subs";
+mongo_client.connect(mongo_sub_url, function(err,db){
+   if(err == null){
+      console.log("[MONGODB]: Failed to connect");
+      return;
+   }
+   console.log("[MONGODB]: Connection success");
+   db.close();
+})
+*/
--- srv/prod-node/server.sh
+++ srv/prod-node/server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+kill -2 `ps aux | grep [m]ongo* | awk '{ print $2 }'`
+mongod --fork --dbpath $PWD/db --port 12345 --logpath $PWD/db/mongodb.log
 node server.js
