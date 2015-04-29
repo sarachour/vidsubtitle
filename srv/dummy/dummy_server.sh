ccdceae46392eece0c90957224376db8997eb549
William M. Leiserson <willtor@mit.edu>
2015-04-29 09:46:20 -0400

    Kill only the dummy_server.py, and not the shell script that's performing the kill.

--- srv/dummy/dummy_server.sh
+++ srv/dummy/dummy_server.sh
@@ -4,7 +4,7 @@ ROOTDIR=$UI_PROJ_DIR
 SRVDIR=$ROOTDIR/srv/dummy
 WEBDIR=$ROOTDIR/site
 
-pkill -f "dummy_server.*" && echo "killed any lingering server processes"
+pkill -f "dummy_server.py" && echo "killed any lingering server processes"
 cd "$WEBDIR"
 nohup python "$SRVDIR/dummy_server.py" "$SRVDIR/server.pem" &> "$SRVDIR/log.txt" 2>&1&
 
