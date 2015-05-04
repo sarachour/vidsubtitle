72cb016813a0cba0d4dc1e9b121b0fb4d2d6293f
William M. Leiserson <willtor@mit.edu>
2015-05-04 17:56:37 -0400

    Make Done button do what it's supposed to do.

--- site/src/edit.js
+++ site/src/edit.js
@@ -65,7 +65,6 @@ function save_data () {
     }
 
     var url = $('#player1').prop('src');
-
     var obj = { data: data,
                 url: url };
 
@@ -75,11 +74,15 @@ function save_data () {
     return obj;
 }
 
-function click_done () {
+function preview_redirect () {
     var data = save_data();
     var nav = new Navigator();
     var purl = nav.portal('edit', 'preview', data);
     nav.redirect(purl);
+}
+
+function click_done () {
+    $('#prompt').fadeIn();
 }
 
 function setup_segment (seg) {
@@ -182,4 +185,7 @@ $("document").ready(function() {
 
     $('#replay_button')[0].disabled = true;
     $('#prev_button')[0].disabled = true;
+
+    $('.preview').click(preview_redirect);
+    $('.cancel').click(function () { $('#prompt').fadeOut(); });
 });
