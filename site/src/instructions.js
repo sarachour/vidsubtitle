076603207f6bd0c1e1be8f57957e6b8363f63bc0
William M. Leiserson <willtor@mit.edu>
2015-05-11 15:46:12 -0400

    Add progress into instructions.

--- site/src/instructions.js
+++ site/src/instructions.js
@@ -1,7 +1,7 @@
-function go_next (i) {
+function go_next (i, j) {
     return function (event) {
         $('#i' + i).hide();
-        $('#i' + (i + 1)).show();
+        $('#i' + j).show();
     }
 }
 
@@ -11,8 +11,9 @@ function click_done () {
 
 $("document").ready(function() {
     // Assign buttons.
-    $('#btn-i1-next').click(go_next(1));
-    $('#btn-i2-next').click(go_next(2));
+    $('#btn-i1-next').click(go_next(1, 2));
+    $('#btn-i2-next').click(go_next(2, 3));
+    $('#btn-i3-next').click(go_next(3, 1));
     $('.btn-skip').click(click_done);
 
     $('#i2').hide();
